cf2bbc975d17061d31dd7c9f14e2dcaeac111ef6
privianet <support@privianet.com>
2024-12-09 16:25:16 +0100

    creation de deploy.php

---
+++ deploy.php
@@ -0,0 +1,27 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Config
+
+set('repository', 'https://github.com/Priviatech/client.domaine-achat.fr.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('prodclientdomaineachat.privianet.com')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/client.domaine-achat.fr')
+    ->set('branch', 'main');
+host('devclientdomaineachat.privianet.com')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/client.domaine-achat.fr')
+    ->set('branch', 'dev');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
